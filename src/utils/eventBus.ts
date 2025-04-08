55213d4a630f85a759afb12f90f7824f79f779a1
snowsun <snowsun@ehsure.com>
2025-04-08 15:36:42 +0800

    [F] 添加使用方法注释

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -5,3 +5,18 @@ interface MittTypes {
 }
 
 export default mitt<MittTypes>()
+
+// 使用方法
+// ComponentA.vue
+// const login = () => {
+//   bus.emit('user-login', { username: 'Alice' })
+// }
+
+// ComponentB.vue
+// onMounted(() => {
+//   bus.on('user-login', handleLogin)
+// })
+
+// onUnmounted(() => {
+//   bus.off('user-login', handleLogin) // 避免内存泄漏
+// })
